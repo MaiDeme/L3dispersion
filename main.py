81d9340464dbfa1d57e878d19eba9b8f8e4825c7
Maïwen Demeulle <maiwen.demeulle@gmail.com>
2022-11-12 23:42:05 +0100

    rajout fonction selection + implantation et dispersion pour les graines dispersées

--- main.py
+++ main.py
@@ -10,5 +10,26 @@ from fonction import *
 
 
 def main():
+    d=5 #le nombre de générations
+    i=0
+    alpha=0     #proportions de graines dispersées
+    N=3         #nombre de graines
+    L=10        #taille du maillage
+    delta=0.1
+    p_ext=0.2  
+    g1=generate_grille(5) #on genere la grille de départ remplie des plantes de quality 1
+    while i <d: #condition fixée pour l'instant le temps de comprendre tout
+        g2=np.zeros((5,5)) #on genre  une grille vide pour y mettre les graines
+        #dispersion (pour l'instant juste les graines dispersées) +implantation
+        g2=implantation_disp(L,g2,alpha,p_ext,N)
+        #donc la on a la grille avec les graines dipersées ET implantés
+
+        #dispersion graines fixées
+
+        #
+
 
     return None
+
+
+main()
