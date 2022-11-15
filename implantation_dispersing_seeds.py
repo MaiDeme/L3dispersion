ad9a1e4a2ac811acab90f1090c4d3ee67bdb4b3f
AlexandrePrad <alexandre.aldolu@gmail.com>
2022-11-15 19:50:14 +0100

    new fonction plus stylé, je pense

--- implantation_dispersing_seeds.py
+++ implantation_dispersing_seeds.py
@@ -3,36 +3,39 @@ import random
 import numpy.random as rd
 
 
-def grille_intermédiaire(L): 
+def implantation_disp(g,alpha,N,proba_installation):
     """
-    créer un tableau de list L*L.
-    """        
-    g=[]
-    for i in np.arange(L):
+    produit une grille intermédiaire vide 
+    remplie la grille avec les graines dispersées implantées.
+    paramètres:
+    g : une grille de taille L*L contenant les plantes gen(n-1)
+    alpha : proportion de graines dispersées
+    N: nombre de graines par plantes
+    proba_installation: valeur de la proba de s'installer 
+
+    return:
+    la grille remplie des plantes (dispersées) implantées
+
+    """  
+    #créer un tableau de list L*L.
+    inte_grille=[]
+    for i in np.arange(len(g)):
         x=[[]]
         j=0
         while j<(L-1):
             x.append([])
             j+=1
-        g.append(x)
-    return g
+        inte_grille.append(x) 
 
 
+    #compte le nombre de plantes sur la grille 
+    nb_plante=0  
+    for i in np.arange(len(g)):
+        for j in np.arange(len(g)):
+            if g[i][j]!=[]:
+                nb_plante+=1
 
-def implantation_disp(p,L,g,alpha,proba_installation):
-    """
-    remplie la grille avec les graines dispersées.
-    paramètres:
-    p : la plante (l'instance de la classe plants étudiée)
-    L : la longeur de la grille
-    g : une grille de taille L*L
-    alpha : proportion de graines dispersées
-
-    return:
-    la grille remplie des plantes (dispersées) implantées
-
-    """        
-    nb_g=int(alpha*N)# le nombre de graines dispersées de la plante
+    nb_g=int(nb_plante*alpha*N)# le nombre total de graines dispersées 
 
     for i in np.arange(nb_g): #boucle qui modélise la dispersion pour chaque graine
         x= random.randint(0, L-1) #choisis aléatoirement une ligne sur la grille
@@ -40,6 +43,6 @@ def implantation_disp(p,L,g,alpha,proba_installation):
         j=rd.binomial(1, proba_installation,) #simule la proba de s'installé sur une case
         
         if j==1:
-            g[x][y].append(plants(1,[x,y])) #si la graine s'installe alors on place une instance dela claas plants dans la case correspondante, il peut y avoir plusieurs 1 sur une case
+            inte_grille[x][y].append(plants(1,[x,y])) #si la graine s'installe alors on place une instance dela claas plants dans la case correspondante, il peut y avoir plusieurs 1 sur une case
 
-    return g
+    return inte_grille
