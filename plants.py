03a61c30a9fae6c02e83e92c24d28d99fdff3e55
MaiDeme <81712833+MaiDeme@users.noreply.github.com>
2022-11-16 16:14:39 +0100

    Update plants.py
    ajout alpha

--- plants.py
+++ plants.py
@@ -11,12 +11,16 @@ class plants:
     Classe d'objects à deux attribus, la qualité q entre 0 et 1 et un couple d'entiers pour les positions des plantes dans la grille.
     """
 
-    def __init__(self, quality, position):
+    def __init__(self, quality, position,alpha):
         self.quality = quality
         self.position = position
+        self.alpha=alpha
 
     def get_position(self):
         return self.position
+    
+    def get_alpha(self):
+        return self.alpha
 
     def get_quality(self):
         return self.quality
@@ -26,4 +30,7 @@ class plants:
 
     def newposition(self, newposition):
         self.position = newposition
+    
+    def newalpha(self,newalpha):
+        self.alpha=newalpha
 
